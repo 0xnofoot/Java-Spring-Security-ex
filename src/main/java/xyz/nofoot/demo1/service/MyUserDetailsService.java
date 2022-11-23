4d7712ef08cecaaa3a53c9fc0049ac68dba5a120
0xnofoto <jnwang99@outlook.com>
2022-11-24 00:15:07 +0800

    jjj

--- src/main/java/xyz/nofoot/demo1/service/MyUserDetailsService.java
+++ src/main/java/xyz/nofoot/demo1/service/MyUserDetailsService.java
@@ -1,12 +1,22 @@
 package xyz.nofoot.demo1.service;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service("userDetailsService")
 public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("role");
+        return new User("nofoot",
+                new BCryptPasswordEncoder().encode("567"), auths);
     }
 }
